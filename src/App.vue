c98ee31f73f43776f7e155fdece71e64d7f8ebc1
xiaoguo3606 <285726161@qq.com>
2017-08-03 22:37:48 +0800

    更新内容

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+  	<page-header></page-header>
+  	<div class="tab">
+  		<div class="tab-item">商品</div>
+  		<div class="tab-item">评论</div>
+  		<div class="tab-item">商家</div>
+  	</div>
+  	<div class="content">
+  		I am the Content
+  	</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+	import pageHeader from './components/pageHeader/pageHeader.vue';
+	
+	export default{
+		components: {
+			pageHeader
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	.tab{
+		display: flex;
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+	}
+	.tab-item{
+		flex: 1;
+		text-align: center ;
+	}
 </style>
---
+++ src/components/pageHeader/pageHeader.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="pageHeader">
+		我是header
+	</div>
+</template>
+
+<script>
+	export default{};
+</script>
+
+<style>
+</style>
